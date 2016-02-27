8e76610a9a4bbdef599056c118a8f89510da2567
Nathaniel Miller <dukeoflaser@gmail.com>
2016-02-27 02:44:07 +0000

    Add Appointment, Doctor, and Patient classes

---
+++ lib/appointment.rb
@@ -0,0 +1,11 @@
+class Appointment
+  attr_reader :date 
+  attr_accessor :doctor, :patient
+  
+  def initialize(date, doctor)
+    @date = date
+    @doctor = doctor
+    # @patient = nil
+    doctor.appointments << self
+  end
+end
